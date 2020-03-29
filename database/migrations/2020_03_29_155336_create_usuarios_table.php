629a8e18846654342e6feb5fe0911f49f941f5e5
leandro96x <46700574+leandro-chile@users.noreply.github.com>
2020-03-29 13:02:40 -0300

    Primer subida de datos

---
+++ database/migrations/2020_03_29_155336_create_usuarios_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+	public function up()
+	{
+		Schema::create('usuarios', function (Blueprint $table) {
+			$table->id();
+			$table->string('nombre')->nullable();
+			$table->string('correo')->unique();
+			$table->string('password');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::dropIfExists('usuarios');
+	}
+}
