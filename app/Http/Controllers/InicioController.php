dfe982c87eadec25d6e7990204e1eae58deb3d67
Leandro Núñez <leandro161996@gmail.com>
2020-04-07 21:37:26 -0400

    Vista y ruta login, arreglos de las vistas.

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class InicioController extends Controller
 {
-	public function index()
+	public function index ()
 	{
 		return view('index');
+	}
+
+	public function login ()
+	{
+		return view('login');
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', 'InicioController@login')->name('login');
+
 Route::get('/', 'InicioController@index')->name('index');
 
 Route::group(['prefix' => 'usuarios'], function() {
