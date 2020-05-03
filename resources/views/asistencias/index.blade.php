0b8d4064d4c70f7540014104b192e6bd43826538
Leandro Núñez <leandro161996@gmail.com>
2020-05-03 17:56:56 -0400

    Nueva vista para mostrar los registros de asistencia.

--- resources/views/asistencias/index.blade.php
+++ resources/views/asistencias/index.blade.php
@@ -1,4 +1,57 @@
 @extends('layout')
 @section('content')
+@section('css')
+<!-- Custom styles for this page -->
+<link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endsection
 
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+	<!-- Page Heading -->
+	<h1 class="h3 mb-2 text-gray-800">Tables</h1>
+	<p class="mb-4">Aqui se puede obserbar los registros de asistencias de los trabajadores registrados. {{-- <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p> --}}
+	<!-- DataTales Example -->
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
+			<h6 class="m-0 font-weight-bold text-primary">Registros de asistencias</h6>
+		</div>
+		<div class="card-body">
+			<div class="table-responsive">
+				<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Position</th>
+							<th>Office</th>
+							<th>Age</th>
+							<th>Start date</th>
+							<th>Salary</th>
+						</tr>
+					</thead>
+					<tfoot>
+						<tr>
+							<th>Name</th>
+							<th>Position</th>
+							<th>Office</th>
+							<th>Age</th>
+							<th>Start date</th>
+							<th>Salary</th>
+						</tr>
+					</tfoot>
+					<tbody>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+@section('js')
+<!-- Page level plugins -->
+<script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<!-- Page level custom scripts -->
+<script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+{{-- $('#dataTable').DataTable(); en el JS listo --}}
+@endsection
 @endsection
