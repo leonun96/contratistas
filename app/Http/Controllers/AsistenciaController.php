06eb05b0c6895abf5ddbbb8b13847cd26e75cd81
Leandro Núñez <leandro161996@gmail.com>
2020-05-02 22:29:14 -0400

    Nueva vista y funcion para asistencias.

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Asistencia;
 
 class AsistenciaController extends Controller
 {
-    //
+	public function index ()
+	{
+		$asistencia = Asistencia::all();
+		return view('asistencias.index')
+		->with('asistencia', $asistencia);
+	}
 }
