72bd86eabcc0f31f4103cd056490ea5ca0071c18
Leandro Núñez <leandro161996@gmail.com>
2020-06-12 02:22:25 -0400

    Nuevo buscador. Nueva carpeta para anticipos.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,14 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 	<!-- Page Heading -->
-	{{-- <h1 class="h3 mb-4 text-gray-800">Página test</h1> --}}
+	<h1 class="h3 mb-4 text-gray-800">Buscador de trabajadores y pagos</h1>
+	<hr>
+	<!-- Search form -->
+	<form class="form-inline active-cyan-4">
+		@csrf
+		<input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
+		<i class="fas fa-search" aria-hidden="true"></i>
+	</form>
 </div>
 <!-- /.container-fluid -->
 @endsection
