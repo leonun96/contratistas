734d2881b356d7f2618c81310a7a4fdbcd969121
Leandro Núñez <leandro161996@gmail.com>
2020-11-29 21:00:36 -0300

    Se ponen los anticipos de la semana solamente en el reporte.

--- app/Exports/PagosExport.php
+++ app/Exports/PagosExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Pagos;
+use App\Anticipos;
 use DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -17,9 +18,13 @@ class PagosExport implements FromView
 	}
 	public function view(): View
 	{
-		$pagos = Pagos::whereMonth('fecha', $this->mes)->with(['empresas','trabajadores','costos','costos.labores'])->get();
+		$pagos = Pagos::whereMonth('fecha', $this->mes)->with(['empresas','trabajadores','trabajadores.anticipos','costos','costos.labores'])->get();
+		$pasada = date_create()->modify('-5 days');
+		$anticipos = Anticipos::whereDate('fecha', '>=', $pasada->format('Y-m-d'))->get();
 		return view('reportes.pagos', [
 			'pagos' => $pagos,
+			'pasada' => $pasada,
+			'anticipos' => $anticipos,
 		]);
 	}
 }
--- resources/views/reportes/pagos.blade.php
+++ resources/views/reportes/pagos.blade.php
@@ -8,7 +8,9 @@
 		<th>Labor</th>
 		<th>Costo</th>
 		<th>Cantidad</th>
-		<th>Total</th>
+		<th>Total pago</th>
+		<th>Anticipos</th>
+		<th>Total final</th>
 	</tr>
 	</thead>
 	<tbody>
@@ -23,6 +25,14 @@
 			<td>{{ $pago->costos->valor }}</td>
 			<td>{{ $pago->cantidad }}</td>
 			<td>{{ $pago->total }}</td>
+			<td>
+				@php
+				$ants = $anticipos->where('trabajadores_id', $pago->trabajadores->id);
+				// $ants->sum('monto');
+				@endphp
+				{{ $ants->sum('monto') ?? 0 }}
+			</td>
+			<td>{{ $pago->total - $ants->sum('monto') }}</td>
 		</tr>
 	@endif
 	@endforeach
